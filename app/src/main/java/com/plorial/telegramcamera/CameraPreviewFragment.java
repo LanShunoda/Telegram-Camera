2da06c335fbc780fa9c24574f6c99ecc4208b385
Plorial <lanshunoda@rambler.ru>
2016-04-29 00:34:56 +0300

    switcher rotate anim

--- app/src/main/java/com/plorial/telegramcamera/CameraPreviewFragment.java
+++ app/src/main/java/com/plorial/telegramcamera/CameraPreviewFragment.java
@@ -1,5 +1,6 @@
 package com.plorial.telegramcamera;
 
+import android.app.Activity;
 import android.app.Fragment;
 import android.content.Context;
 import android.content.pm.PackageManager;
@@ -8,9 +9,15 @@ import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.util.Log;
 import android.view.LayoutInflater;
+import android.view.Surface;
 import android.view.View;
 import android.view.ViewGroup;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
 import android.widget.FrameLayout;
+import android.widget.ImageButton;
+
+import java.io.IOException;
 
 /**
  * Created by plorial on 4/28/16.
@@ -21,27 +28,42 @@ public class CameraPreviewFragment extends Fragment{
 
     private Camera camera;
     private CameraPreview preview;
+    private int currentCameraId;
+    private FrameLayout frameLayout;
 
     @Nullable
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.camera_preview_fragment, container, false);
-        camera = getCameraInstance();
+        currentCameraId = Camera.CameraInfo.CAMERA_FACING_BACK;
+        camera = getCameraInstance(currentCameraId);
 
         preview = new CameraPreview(getActivity(), camera);
-        FrameLayout frameLayout = (FrameLayout) view.findViewById(R.id.camera_preview);
+
+        frameLayout = (FrameLayout) view.findViewById(R.id.camera_preview);
         frameLayout.addView(preview);
 
         frameLayout.setOnTouchListener(new FlipperOnTouchListener(view));
+        final Animation animationRotate = AnimationUtils.loadAnimation(
+                getActivity(), R.anim.rotate);
+
+        final ImageButton switchButton = (ImageButton) view.findViewById(R.id.switchButton);
+
+        switchButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                switchButton.startAnimation(animationRotate);
+            }
+        });
 
         return view;
     }
 
-    private Camera getCameraInstance(){
+    private Camera getCameraInstance(int id){
         Camera c = null;
         try {
             if(checkCameraHardware(getActivity())) {
-                c = Camera.open();
+                c = Camera.open(id);
             }
         }
         catch (Exception e){
