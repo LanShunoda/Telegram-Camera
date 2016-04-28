db9f0747b1a2661294c29b8ea63845ca34c8cb06
Plorial <lanshunoda@rambler.ru>
2016-04-28 15:55:06 +0300

    camera preview fragment added

--- app/src/main/java/com/plorial/telegramcamera/MainActivity.java
+++ app/src/main/java/com/plorial/telegramcamera/MainActivity.java
@@ -2,63 +2,27 @@ package com.plorial.telegramcamera;
 
 
 import android.app.Activity;
-import android.content.Context;
-import android.content.pm.PackageManager;
-import android.hardware.Camera;
+import android.app.FragmentTransaction;
 import android.os.Bundle;
-import android.util.Log;
 import android.view.Window;
 import android.view.WindowManager;
-import android.widget.FrameLayout;
 
 public class MainActivity extends Activity {
 
     private static final String TAG = MainActivity.class.getSimpleName();
 
-    private Camera camera;
-    private CameraPreview preview;
-
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
         requestWindowFeature(Window.FEATURE_NO_TITLE);
         getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);
 
         setContentView(R.layout.activity_main);
-
-        camera = getCameraInstance();
-
-        preview = new CameraPreview(this, camera);
-        FrameLayout frameLayout = (FrameLayout) findViewById(R.id.camera_preview);
-        frameLayout.addView(preview);
-    }
-
-    private Camera getCameraInstance(){
-        Camera c = null;
-        try {
-            if(checkCameraHardware(this)) {
-                c = Camera.open();
-            }
-        }
-        catch (Exception e){
-            Log.e(TAG, "Camera isn't available");
-        }
-        return c;
-    }
-
-    private boolean checkCameraHardware(Context context) {
-        if (context.getPackageManager().hasSystemFeature(PackageManager.FEATURE_CAMERA)){
-            return true;
-        } else {
-            return false;
-        }
-    }
-
-    @Override
-    protected void onPause() {
-        super.onPause();
-        if (camera != null)
-            camera.release();
-        camera = null;
+        CameraPreviewFragment fragment = new CameraPreviewFragment();
+        FragmentTransaction transaction = getFragmentManager().beginTransaction();
+        transaction.add(R.id.fragment_container, fragment);
+        transaction.addToBackStack(null);
+        transaction.commit();
     }
 }
